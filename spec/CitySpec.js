342cbe13611f1782bb8d6a2dc1e04bfb56123488
wardymate <chriswarddev@gmail.com>
2015-03-05 11:43:11 +0000

    city object is finished and passes"

--- spec/CitySpec.js
+++ spec/CitySpec.js
@@ -1,18 +1,38 @@
 describe('City', function(){
-
   var city;
-
-  it('has a temperature', function(){
+  beforeEach(function(){
     city = new City;
-    expect(city.temp).toEqual(20);
   });
 
-  it('can set its temperature', function(){
-    city = new City;
-    city.setTemp(22);
-    expect(city.temp).toEqual(22);
+  describe('knows that it', function(){
+
+    it('has a temperature', function(){
+      expect(city.temp).toEqual(20);
+    });
+
+    it('can set its temperature', function(){
+      city.setTemp(22);
+      expect(city.temp).toEqual(22);
+    });
+
+    it('has an outlook', function(){
+      expect(city.outlook).toEqual('sunny')
+    });
+
+    it('can set its outlook', function(){
+      city.setOutlook('rainy');
+      expect(city.outlook).toEqual('rainy');
+    });
+
+    it('has a name',function(){
+      expect(city.cityName).toEqual("london");
+    });
+
+    it('can set its name', function() {
+      city.setName('paris');
+      expect(city.cityName).toEqual("paris");
+    });
+
   });
-
-
 
 });
--- src/City.js
+++ src/City.js
@@ -1,3 +1,18 @@
 var City = function(){
   this.temp = 20;
+  this.outlook = 'sunny';
+  this.cityName = 'london';
+
+};
+
+City.prototype.setTemp = function(newTemp) {
+  this.temp = newTemp;
+};
+
+City.prototype.setOutlook = function(newOutlook) {
+  this.outlook = newOutlook;
+};
+
+City.prototype.setName = function(newName) {
+  this.cityName = newName;
 };
